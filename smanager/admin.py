6b8c59741bc8008c7758ca4b9d2d0961cd21df1b
Jonathan mwenze <jonathanmwenze@gmail.com>
2021-07-20 20:29:46 +0200

    Ajout des models dans admin

--- smanager/admin.py
+++ smanager/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Artiste, Album, Song
 
-# Register your models here.
+
+
+admin.site.site_header = "Smanager"
+admin.site.site_title = "Song Manager"
+admin.site.index_title = "Welcome To Smanager"
+
+@admin.register(Artiste)
+class ArtisteAdmin(admin.ModelAdmin):
+	pass
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+	pass
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+	pass
+
