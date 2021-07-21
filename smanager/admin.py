6dba3d96bc1f3ee162b1e07a35041a9f3fb2b730
Jonathan mwenze <jonathanmwenze@gmail.com>
2021-07-21 20:06:04 +0200

    Ajout des models dans admin

--- smanager/admin.py
+++ smanager/admin.py
@@ -9,13 +9,19 @@ admin.site.index_title = "Welcome To Smanager"
 
 @admin.register(Artiste)
 class ArtisteAdmin(admin.ModelAdmin):
+	list_display = ('prenom', 'nom', 'birthday', 'country', 'town')
+	list_filter = ('birthday', 'country', 'town')
 	pass
 
 @admin.register(Album)
 class AlbumAdmin(admin.ModelAdmin):
+	list_display = ('titre', 'artiste.prenom', 'release_date')
+	list_filter = ('titre', 'artiste', 'release_date')
 	pass
 
 @admin.register(Song)
 class SongAdmin(admin.ModelAdmin):
+	list_display = ('song_title', 'album', 'duration', 'lyrics')
+	list_filter = ('album', 'duration')
 	pass
 
